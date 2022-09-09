a645b006ac18cf4b5d6cda701f231e149065413e
tim-jude <timothyplay1999@gmail.com>
2022-09-09 05:02:59 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main orint in push
+*
+* use main function
+* print "Programming is like building a multilingual puzzle
+* return: 0
+*/
+int main(void)
+{
+   puts("/"Programming is like building a multilingual puzzle");
+   return (0);
+}
