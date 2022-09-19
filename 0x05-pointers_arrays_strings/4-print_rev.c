48c6f901a131d6dcb260d72a7459320bc635e363
tim-jude <timothyplay1999@gmail.com>
2022-09-19 15:15:40 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_rev - string in reverse
+* @s: string to be reversed
+* Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
