e922b796fed84b9746f1b23966b8b7097ef56a6e
tim-jude <timothyplay1999@gmail.com>
2022-09-21 15:30:36 -0700

    music

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,16 +2,17 @@
 #define MAIN_H
 #include <stdio.h>
 /**
-* void prototypes()
-* int prototypes()
-* char prototypes()
-*/
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+char *leet(char *);
 char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 
 #endif /*MAIN_H*/
