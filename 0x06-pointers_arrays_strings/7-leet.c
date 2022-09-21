a057c6f2958ab295e046057c76c4159d279541fa
tim-jude <timothyplay1999@gmail.com>
2022-09-21 16:03:41 -0700

    mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,46 @@
 #include "main.h"
 
 /**
- * leet - encodes string to 1337
- * @s: string
+ * leet - encoding
+ * @str: string
  *
  * Return: add of s
  */
-char *leet(char *s)
-}
-	int i, j;
-	char a[] = "aAeEoOtTlL";
-	char b[] = "4433007711";
 
-	for (i = 0; *(s + i); i++)
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			if (a[j] == *(s + i))
-				*(s + i) = b[j];
-		}
+		str[i] = transform(str[i]);
+		i++;
 	}
-	return (s);
+	return (str);
+}
+
+/**
+ * transform - function to map letter to leet encoding
+ * @x: char to be encoded
+ *
+ * Return: encoded char
+ */
+
+char transform(char x)
+{
+	char mapping_low[0] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[0] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 0)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break;
+		}
+		i++;
+	}
+	return (replacement);
 }
