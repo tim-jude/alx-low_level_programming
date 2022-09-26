990fcc286772b02d5be025f8471665d41b1af246
tim-jude <timothyplay1999@gmail.com>
2022-09-26 13:12:13 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - gets substring
+ * @haystack: string to search needle
+ * @needle: substring
+ *
+ * Return: pointer to beginning of needle or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
